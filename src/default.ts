f7f4a566ae05101aca22be5c40d298e18388cf6a
Marx2014 <103162082+Marx2014@users.noreply.github.com>
2023-03-24 15:51:28 +0800

    Update default.ts

--- src/default.ts
+++ src/default.ts
@@ -7,6 +7,6 @@ export const defaultSetting = {
 }
 
 export const defaultMessage = `
-- 本项目基于GPT 3.5 Turbo语言模型。
-- 如果本项目对你有所帮助，可以给作者[打赏](https://note.youdao.com/s/WGXN3Q6C)
+- 我是一个AI智能助手，能够为您提供最准确和实用的答案，解决您在日常生活和工作中遇到的各种问题。我不仅能够提供简单的指导，还可以为您提供专业的技术支持和咨询服务。让我成为您的贴心助手，为您的生活和工作带来更多便利和乐趣。
+- 本项目基于GPT 3.5 Turbo语言模型，如果本项目对你有所帮助，可以给作者[打赏](https://note.youdao.com/s/WGXN3Q6C)
 `
